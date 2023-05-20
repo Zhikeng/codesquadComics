b9ee5fb7d20b89f7636ca0a4d2beb0d3b959a854
Zhikeng <114634385+Zhikeng@users.noreply.github.com>
2023-05-20 17:30:12 -0400

    Update user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -44,7 +44,7 @@ passport.deserializeUser(function(user,cb)
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/admin-console"
+  callbackURL: "https://cautious-lime-lizard.cyclic.app/auth/google/admin-console"
 },
 function(accessToken, refreshToken, profile, cb) {
   User.findOrCreate({ googleId: profile.id }, function (err, user) {
